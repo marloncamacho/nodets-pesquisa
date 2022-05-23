1727aa9ca15b2077e66cc748c6cc185475679da2
Marlon Camacho <marlonalexandre@gmail.com>
2022-05-23 17:11:30 -0300

    base do projeto

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # nodets-pesquisa
+Projeto de Estudo feito com NODE + Typescript
+Estabelecendo rotas e mecanismos de pesquisar elementos na página
+
+## Pré-requisitos globais:
+'npm install -g nodemon typescript ts-node'
+
+## Instalação
+'npm install'
+
+## Inicializar o projeto 
+'npm run start-dev'
---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express, {Request, Response} from 'express';
+import mustache from 'mustache-express';
+import dotenv from 'dotenv';
+import path from 'path';
+import mainRouters from './routers/index'
+
+dotenv.config();
+const server = express();
+
+
+server.set('view engine', 'mustache');
+server.set('views', path.join(__dirname, 'views'));
+server.engine('mustache', mustache());
+
+server.use(express.static(path.join(__dirname, '../public')));
+
+//ROTAS
+server.use(mainRouters);
+server.use((req: Request, res: Response) => {
+    res.send("Página Não Encontrada")
+});
+
+server.listen(process.env.PORT);
